80f7e4b3bf91024ff63db7d2d815a96f4880149a
Asteria <2252689480@qq.com>
2022-10-15 13:50:10 +0800

    2022.10.15

---
+++ build/CMakeFiles/CMake_demo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMake_demo.dir/CMake_demo.cpp.obj"
+  "CMakeFiles/CMake_demo.dir/CMake_demo.cpp.obj.d"
+  "CMake_demo.exe"
+  "CMake_demo.exe.manifest"
+  "CMake_demo.pdb"
+  "libCMake_demo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMake_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
