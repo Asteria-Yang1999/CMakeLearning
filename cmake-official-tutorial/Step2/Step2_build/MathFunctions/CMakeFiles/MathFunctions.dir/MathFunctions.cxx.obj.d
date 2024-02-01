7e0ecb88d8c5e362e6ad9085317ccbd33eb53571
Asteria <2252689480@qq.com>
2024-02-01 14:16:01 +0800

    2024.2.1 add a library

---
+++ cmake-official-tutorial/Step2/Step2_build/MathFunctions/CMakeFiles/MathFunctions.dir/MathFunctions.cxx.obj.d
@@ -0,0 +1,4 @@
+MathFunctions/CMakeFiles/MathFunctions.dir/MathFunctions.cxx.obj: \
+ D:\Codes\C++\CMakeLearning\cmake-official-tutorial\Step2\MathFunctions\MathFunctions.cxx \
+ D:\Codes\C++\CMakeLearning\cmake-official-tutorial\Step2\MathFunctions\MathFunctions.h \
+ D:\Codes\C++\CMakeLearning\cmake-official-tutorial\Step2\MathFunctions\mysqrt.h
